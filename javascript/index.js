f50f4c217cd6eb65f2402f1bc70166df813e88ae
akshada4 <akshadasipsap@gmail.com>
2021-08-17 03:27:11 +0530

    added footer and content

--- javascript/index.js
+++ javascript/index.js
@@ -17,6 +17,37 @@ const displayForm = () => {
 	form.setAttribute('style', 'display: block');
 }
 
+const getNewCard = (e) => {
+	const card = e.currentTarget;
+	const newCard = card.querySelector('.hidden-card');
+	return newCard;
+}
+
+const displayHiddenCard = (e) => {
+	const hiddenCard = getNewCard(e);
+	hiddenCard.setAttribute('style', 'display:block')
+}
+
+const hideHiddenCard = (e) => {
+	const displayedCard = getNewCard(e);
+	displayedCard.setAttribute('style','display:none');
+}
+
+const addCardImg = (e) => {
+	const card = e.currentTarget;
+	const newCard= card.querySelector('.new-card');
+	const cardImg = card.querySelector('img');
+	cardImg.setAttribute('style', 'display:inline-block');
+	newCard.remove();
+}
+
+const cardsEventListener = (cards) => {
+	for(let i = 0; i<cards.length; i++){
+		cards[i].addEventListener('mouseenter', displayHiddenCard);
+		cards[i].addEventListener('mouseleave', hideHiddenCard);
+	}
+}
+
 const email = document.querySelector('#email');
 const firstName = document.querySelector('#first-name');
 const lastName =  document.querySelector('#last-name');
@@ -31,3 +62,7 @@ lastName.addEventListener('focusout', focusOutChangeLabel);
 
 const contactBtn = document.querySelector('#contact');
 contactBtn.addEventListener('click', displayForm);
+
+const cards = document.querySelectorAll('.card');
+let changedCard = true;
+cardsEventListener(cards);
